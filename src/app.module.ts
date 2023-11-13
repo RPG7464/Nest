5a35ff29ead00e8adda13cf87360906d05bad986
Rafael Peñaranda <rpg7464@gmail.com>
2023-11-13 16:41:15 -0500

    configuration 2

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,7 @@
 import { Module } from '@nestjs/common';
-import { CatsModule } from './cats/cats.module';
-import { AuthModule } from './auth/auth.module';
-import { UsersModule } from './users/users.module';
 import { APP_PIPE } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common/pipes';
 import { ConfigModule, ConfigService } from '@nestjs/config';
-import { DatabaseModule } from './database/database.module';
 import configuration from './config/configuration';
 import { ValidationSchema } from './config/validationSchema';
 import { ModulesModule } from './modules/modules.module';
